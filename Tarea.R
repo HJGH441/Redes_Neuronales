a6a01347d9821cb8e0fe66805623b3bad715cff5
Héctor Jesús González Haro <hectorjesusharo441@gmail.com>
2023-03-31 19:17:19 -0600

    Update Tarea.R

--- Tarea.R
+++ Tarea.R
@@ -1,7 +1,7 @@
 ## Creating index variable 
 
 # Read the Data
-data = read.csv("cereals.csv", header=T)
+data = read.csv("C:/Users/Dell/Downloads/cereals.csv", header=T)
 
 # Random sampling
 samplesize = 0.60 * nrow(data)
@@ -18,51 +18,6 @@ datatest = data[ -index, ]
 max = apply(data , 2 , max)
 min = apply(data, 2 , min)
 scaled = as.data.frame(scale(data, center = min, scale = max - min))
-
-Introduction
-Neural network is an information-processing machine and can be viewed as analogous to human nervous system. Just like human nervous system, which is made up of interconnected neurons, a neural network is made up of interconnected information processing units. The information processing units do not work in a linear manner. In fact, neural network draws its strength from parallel processing of information, which allows it to deal with non-linearity. Neural network becomes handy to infer meaning and detect patterns from complex data sets.
-
-Neural network is considered as one of the most useful technique in the world of data analytics. However, it is complex and is often regarded as a black box, i.e. users view the input and output of a neural network but remain clueless about the knowledge generating process. We hope that the article will help readers learn about the internal mechanism of a neural network and get hands-on experience to implement it in R.
-
-
-
-Table of Contents
-The Basics of Neural Network
-Fitting Neural Network in R
-Cross Validation of a Neural Network
-
-
-The Basics of Neural Network
-A neural network is a model characterized by an activation function, which is used by interconnected information processing units to transform input into output. A neural network has always been compared to human nervous system. Information in passed through interconnected units analogous to information passage through neurons in humans. The first layer of the neural network receives the raw input, processes it and passes the processed information to the hidden layers. The hidden layer passes the information to the last layer, which produces the output. The advantage of neural network is that it is adaptive in nature. It learns from the information provided, i.e. trains itself from the data, which has a known outcome and optimizes its weights for a better prediction in situations with unknown outcome.
-
-A perceptron, viz. single layer neural network, is the most basic form of a neural network.  A perceptron receives multidimensional input and processes it using a weighted summation and an activation function. It is trained using a labeled data and learning algorithm that optimize the weights in the summation processor. A major limitation of perceptron model is its inability to deal with non-linearity. A multilayered neural network overcomes this limitation and helps solve non-linear problems. The input layer connects with hidden layer, which in turn connects to the output layer. The connections are weighted and weights are optimized using a learning rule.
-
-
-Become a Full Stack Data Scientist
-Transform into an expert and significantly impact the world of data science.
-There are many learning rules that are used with neural network:
-  
-  a) least mean square;
-b) gradient descent;
-c) newton’s rule;
-d) conjugate gradient etc.
-
-The learning rules can be used in conjunction with backpropgation error method. The learning rule is used to calculate the error at the output unit. This error is backpropagated to all the units such that the error at each unit is proportional to the contribution of that unit towards total error at the output unit.  The errors at each unit are then used to optimize the weight at each connection. Figure 1 displays the structure of a simple neural network model for better understanding.
-
-
-
-
-
-Figure 1 A simple neural network model
-
-
-
-Fitting Neural Network in R
-Now we will fit a neural network model in R. In this article, we use a subset of cereal dataset shared by Carnegie Mellon University (CMU). The details of the dataset are on the following link: http://lib.stat.cmu.edu/DASL/Datafiles/Cereals.html. The objective is to predict rating of the cereals variables such as calories, proteins, fat etc. The R script is provided side by side and is commented for better understanding of the user. .  The data is in .csv format and can be downloaded by clicking: cereals.
-
-Please set working directory in R using setwd( ) function, and keep cereal.csv in the working directory. We use rating as the dependent variable and calories, proteins, fat, sodium and fiber as the independent variables. We divide the data into training and test set. Training set is used to find the relationship between dependent and independent variables while the test set assesses the performance of the model. We use 60% of the dataset as training set. The assignment of the data to training and test set is done using random sampling. We perform random sampling on R using sample ( ) function. We have used set.seed( ) to generate same random sample everytime and   maintain consistency. We will use the index variable while fitting neural network to create training and test data sets. The R script is as follows:
-  
-  
   
   ## Creating index variable 
   
@@ -79,18 +34,12 @@ datatrain = data[ index, ]
 datatest = data[ -index, ]
 
 
-Now we fit a neural network on our data. We use neuralnet library for the analysis. The first step is to scale the cereal dataset. The scaling of data is essential because otherwise a variable may have large impact on the prediction variable only because of its scale. Using unscaled may lead to meaningless results. The common techniques to scale data are: min-max normalization, Z-score normalization, median and MAD, and tan-h estimators. The min-max normalization transforms the data into a common range, thus removing the scaling effect from all the variables. Unlike Z-score normalization and median and MAD method, the min-max method retains the original distribution of the variables. We use min-max normalization to scale the data. The R script for scaling the data is as follows.
-
-
-
 ## Scale data for neural network
 
 max = apply(data , 2 , max)
 min = apply(data, 2 , min)
 scaled = as.data.frame(scale(data, center = min, scale = max - min))
 
-
-The scaled data is used to fit the neural network. We visualize the neural network with weights for each of the variable. The R script is as follows.
 
 
 
